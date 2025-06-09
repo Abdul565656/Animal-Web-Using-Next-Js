821c9f03d67e8b09fcb606e356954f7241b708a7
abdul565656 <abdullahkashif12b3@gmailcom>
2025-06-10 02:00:45 +0500

    -- fix: responsive issue in Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="bg-foco-blue text-white text-center text-[11px] sm:text-sm py-2 sm:py-3 px-2 sm:px-4">
         <p className="max-w-screen-md mx-auto leading-tight sm:leading-normal">
           On your first order{" "}
-          <span className="font-bold bg-foco-orange px-2 py-1 rounded-md inline-block">
+          <span className="font-bold bg-foco-orange px-2 py-1 rounded-lg inline-block">
             30% OFF
           </span>{" "}
           | For all products buy now and get the offer
--- src/components/widgets/StatsSection.tsx
+++ src/components/widgets/StatsSection.tsx
@@ -60,3 +60,4 @@ const StatsSection = () => {
 };
 
 export default StatsSection;
+
